12b343c12513439347626df1b3e72f901d3b1fbd
SatvikSingh <64217296+SatvikSingh@users.noreply.github.com>
2021-07-02 00:23:10 +0530

    boss character animations

---
+++ Horror_Survival/Assets/Scripts/BossShoots.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossShoots : MonoBehaviour
+{
+    [SerializeField] Animator anim;
+    [SerializeField] Transform shootScript;
+
+    private bool canShoot = false;
+
+
+    void Start()
+    {
+        shootScript.GetComponent<SimpleShoot1>().enabled = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            canShoot = true;
+            anim.SetTrigger("AimShoot");
+            StartCoroutine(ShootPlayer());
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            canShoot = false;
+            StartCoroutine(ShootPlayer());
+        }
+    }
+
+    IEnumerator ShootPlayer()
+    {
+        yield return new WaitForSeconds(1.0f);
+        if (canShoot == true)
+        {
+            shootScript.GetComponent<SimpleShoot1>().enabled = true;
+        }
+        if (canShoot == false)
+        {
+            shootScript.GetComponent<SimpleShoot1>().enabled = false;
+        }
+    }
+}
